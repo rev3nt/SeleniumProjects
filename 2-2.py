55b976d2896a32f02dac7a719f682d2a4925aa7c
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-11 12:11:12 +0300

    Initial commit

---
+++ 2-2.py
@@ -0,0 +1,65 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# Настройки перед запуском браузера
+options = webdriver.ChromeOptions()
+# Опция, позволяющая сохранять браузер открытым после выполнения скрипта
+options.add_experimental_option('detach', True)
+#Отключение всплывающего окна с уведомлением о том, что введенный пароль есть в слитых базах данных
+options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+
+# Создание экземпляра драйвера с автоматической установкой последней версии
+driver = webdriver.Chrome(options=options)
+# Базовый url, с которым взаимодействует скрипт
+base_url = "https://www.saucedemo.com/"
+# Открытие ссылки в браузере
+driver.get(base_url)
+# Развертывание окна браузера на полный экран
+driver.maximize_window()
+
+# Сохраняем поле ввода логина в переменную
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+# Вписываем неверный логин в поле
+user_name.send_keys("standard_user")
+print("Логин введен")
+
+# Сохраняем поле ввода пароля в переменную
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+# Вводим неверный пароль в поле
+password.send_keys("secret_sauce")
+print("Пароль введен")
+
+# Находим кнопку логина по локатору
+login_button = driver.find_element(By.XPATH, "//input[@id='login-button']")
+# Нажимаем кнопку логина
+login_button.click()
+print("Кнопка логина нажата")
+
+# Сохраняем название и цену первого товара
+product_title = driver.find_element(By.XPATH, "//*[@id='item_4_title_link']/div").text
+product_price = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div").text
+# Добавляем первый товар в корзину
+driver.find_element(By.XPATH, "//*[@id='add-to-cart-sauce-labs-backpack']").click()
+print("Товар добавлен в корзину")
+
+# Переходим на страницу корзины
+driver.find_element(By.XPATH, "//*[@id='shopping_cart_container']/a").click()
+print("Переход в корзину")
+
+time.sleep(1)
+
+# Переходим назад в каталог
+driver.back()
+
+time.sleep(1)
+
+# Переходим вперед в корзину
+driver.forward()
+
+time.sleep(2)
+
+# Закрываем браузер
+driver.close()
