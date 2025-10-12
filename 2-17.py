5df9bbb1dd715ea588e6b99d319fa5fb76e99d63
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-12 22:01:39 +0300

    Initial commit

---
+++ 2-17.py
@@ -0,0 +1,147 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from faker import Faker
+
+
+download_path = "C:\\Users\\user\\PycharmProjects\\SeleniumRefresh\\download_files\\"
+# Настройки перед запуском браузера
+options = webdriver.ChromeOptions()
+# Опция, позволяющая сохранять браузер открытым после выполнения скрипта
+options.add_experimental_option('detach', True)
+# Отключение всплывающего окна с уведомлением о том, что введенный пароль есть в слитых базах данных
+options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+
+# Создание экземпляра драйвера с автоматической установкой последней версии
+driver = webdriver.Chrome(options=options)
+# Базовый url, с которым взаимодействует скрипт
+base_url = "https://www.saucedemo.com/"
+# Открытие ссылки в браузере
+driver.get(base_url)
+# Развертывание окна браузера на полный экран
+driver.maximize_window()
+
+fake = Faker()
+
+menu_dict = {'1': 'Sauce Labs Backpack', '2': "Sauce Labs Bike Light", '3': "Sauce Labs Bolt T-Shirt",
+             '4': "Sauce Labs Fleece Jacket", '5': "Sauce Labs Onesie", '6': 'Test.allTheThings() T-Shirt (Red)")”'}
+
+print("Выберете опцию из меню")
+
+# Выводим пользовательское меню
+for item in menu_dict:
+    print(f'{item}: {menu_dict[item]}')
+
+# Просим ввод до момента, пока не будет выбрана валидная опция
+while True:
+    user_input = input("Выбери один из следующих товаров и укажи его номер: ")
+
+    if user_input not in menu_dict:
+        print("Вы ввели некорректную опцию")
+    else:
+        break
+
+time.sleep(2)
+
+# Вводим логин
+login_field = driver.find_element(By.ID, "user-name")
+login_field.send_keys('standard_user')
+
+# Вводим пароль
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys('secret_sauce')
+
+# Логинимся
+driver.find_element(By.ID, "login-button").click()
+
+# Сохраняем выбор пользователя, он будет использоваться для проведения тестов
+users_choice = menu_dict[user_input]
+
+print(f'Выбрана опция {users_choice}')
+
+# Находим карточку товара по названию товара, находим цену из контейнера с товаром
+product_container = driver.find_element(By.XPATH, f"//div[text()='{users_choice}']/ancestor::div[@class='inventory_item_description']")
+product_price = product_container.find_element(By.CLASS_NAME, "inventory_item_price").text
+print(f'Цена выбранного товара: {product_price}')
+
+# Находим в контейнере кнопку для добавления в корзине
+add_to_cart_button = product_container.find_element(By.XPATH, ".//button[contains(text(), 'Add to cart')]")
+add_to_cart_button.click()
+print("Товар добавлен в корзину")
+
+time.sleep(2)
+
+# Переходим в корзину
+cart_button = driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']")
+cart_button.click()
+print("Переход в корзину")
+
+# Извлекаем информацию о товаре в корзине
+cart_title = driver.find_element(By.XPATH, "//div[@class='inventory_item_name']").text
+cart_price = driver.find_element(By.XPATH, "//div[@class='inventory_item_price']").text
+print(cart_title)
+print(cart_price)
+
+# Сверяем с данными, введенными пользователем и указанными на главной странице
+assert cart_title == users_choice
+assert cart_price == product_price
+print("Содержимое корзины соответствует пользовательскому выбору")
+
+time.sleep(2)
+
+# Переходим на страницу доставки
+driver.find_element(By.XPATH, "//button[@id='checkout']").click()
+
+# Генерируем данные о пользователе
+first_name = fake.first_name()
+last_name = fake.last_name()
+postal_code = fake.postcode()
+
+# Находим локаторы полей для ввода данных
+first_name_filed = driver.find_element(By.XPATH, "//input[@id='first-name']")
+last_name_filed = driver.find_element(By.XPATH, "//input[@id='last-name']")
+postal_code_field = driver.find_element(By.XPATH, "//input[@id='postal-code']")
+
+# Вводим сгенерированные данные
+first_name_filed.send_keys(first_name)
+last_name_filed.send_keys(last_name)
+postal_code_field.send_keys(postal_code)
+
+time.sleep(2)
+
+# Переходим на страницу обзора заказа
+driver.find_element(By.XPATH, "//input[@id='continue']").click()
+print("Переход на страницу с обзором заказа")
+
+# Находим поля с названием, ценой и ценой заказа без налога
+checkout_title = driver.find_element(By.XPATH, "//div[@class='inventory_item_name']").text
+checkout_price = driver.find_element(By.XPATH, "//div[@class='inventory_item_price']").text
+total_price = driver.find_element(By.XPATH, "//div[@class='summary_subtotal_label']").text
+total_price = total_price.replace('Item total: ', '')
+
+# Проверяем на соответствие оригинальным данным
+assert checkout_title == users_choice
+assert checkout_price == product_price
+assert total_price == product_price
+print("Информация о заказе в обзоре соответствует выбранным опциям")
+
+time.sleep(2)
+
+# Подтверждаем заказ
+driver.find_element(By.XPATH, "//button[@id='finish']").click()
+
+# Сохраняем сообщение об успешном оформлении заказа
+complete_text = 'Thank you for your order!'
+
+# Ищем сообщение на странице
+complete_title = driver.find_element(By.XPATH, f"//h2[contains(text(), '{complete_text}')]").text
+
+# Подтверждаем успешность созданного заказа
+assert complete_title == complete_text
+print("Заказ был успешно сделан")
+
+time.sleep(3)
+
+# Выходим из браузера
+driver.quit()
