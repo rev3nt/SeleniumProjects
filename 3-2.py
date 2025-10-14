179ceb0d704598db14e4b6d8bc45f7ed1145babb
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-14 15:48:08 +0300

    Initial commit

---
+++ 3-2.py
@@ -0,0 +1,60 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Test:
+    def test_login_and_adding_in_cart(self):
+        # Настройки перед запуском браузера
+        options = webdriver.ChromeOptions()
+        # Опция, позволяющая сохранять браузер открытым после выполнения скрипта
+        options.add_experimental_option('detach', True)
+        # Отключение всплывающего окна с уведомлением о том, что введенный пароль есть в слитых базах данных
+        options.add_experimental_option("prefs", {"profile.password_manager_leak_detection": False})
+
+        # Создание экземпляра драйвера с автоматической установкой последней версии
+        driver = webdriver.Chrome(options=options)
+        # Базовый url, с которым взаимодействует скрипт
+        base_url = "https://www.saucedemo.com/"
+        # Открытие ссылки в браузере
+        driver.get(base_url)
+        # Развертывание окна браузера на полный экран
+        driver.maximize_window()
+
+        login_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='user-name']")))
+        login_input.send_keys("standard_user")
+        print('Логин введен')
+
+        password_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))
+        password_input.send_keys("secret_sauce")
+        print("Пароль введен")
+
+        login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='login-button']")))
+        login_button.click()
+        print("Кнопка логина нажата")
+
+        add_to_cart_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")))
+        add_to_cart_button.click()
+        print("Товар добавлен в корзину")
+
+        go_cart_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@class='shopping_cart_link']")))
+        go_cart_button.click()
+        print("Переход в корзину")
+
+        success_title = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='title']")))
+        success_title_text = success_title.text
+
+        assert success_title_text == "Your Cart"
+        print("Тест успешен")
+
+        time.sleep(2)
+
+        driver.close()
+
+
+tester = Test()
+
+tester.test_login_and_adding_in_cart()
